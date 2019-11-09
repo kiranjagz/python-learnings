b589dea976737c16cfbcce8aa24a948d02cd0557
Kiran Jagathlal <kiranjagz@gmail.com>
2019-11-09 10:06:41 +0200

    first push

---
+++ basics.py
@@ -0,0 +1,7 @@
+import datetime
+mynow = datetime.datetime.now()
+print("hello", mynow)
+
+mynumber = 1
+mystring = "string"
+print(mynumber,mystring)
---
+++ list.py
@@ -0,0 +1,16 @@
+alist = list(range(1,10))
+rainfall = [10.1, 9, "no data", [1,2,3]]
+print(alist.__contains__(23))
+print(alist)
+print(rainfall)
+
+student_grades = [3.4,9,8,23,4.1]
+
+print(student_grades)
+
+total = sum(student_grades)
+length = len(student_grades)
+average = total / length
+print(total)
+print(length)
+print(average)
