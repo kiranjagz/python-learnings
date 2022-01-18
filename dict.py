2ce214deaa1ce99b48e3c138fe9d732d330f3fcc
Kiran <kiranjagz@gmai.com>
2022-01-18 21:39:41 +0200

    Added dictionary

---
+++ dict.py
@@ -0,0 +1,7 @@
+temperatures = [ 9.4, 8.8, 7.7]
+student_grades = { "Mary": 10.1, "Kiran": 10.3}
+
+mysum = sum(student_grades.values())
+length = len(student_grades)
+mean  = mysum / length
+print("Result: ", mean)
