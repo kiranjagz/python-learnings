e53f1273e7e5464a1c7cca10ffca60eb640ae52b
Kiran <kiranjagz@gmai.com>
2022-01-19 22:31:26 +0200

    Add more examples of conversions

--- convert_dataTypes/convert.py
+++ convert_dataTypes/convert.py
@@ -1,5 +1,14 @@
 # from tuple to list:
 data = (1, 2, 3)
 aList = list(data)
-
 print(aList)
+
+# from list to tuple:
+data = [1, 2, 3]
+aTuple = tuple(data)
+print(aTuple)
+
+# from list to dictionary:
+data = [["name", "John"], ["surname", "smith"]]
+aDict = dict(data)
+print(aDict)
