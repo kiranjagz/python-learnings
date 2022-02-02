c7d1581a90fad4aae823153e33530970332d6858
Kiran <kiranjagz@gmai.com>
2022-02-02 21:15:08 +0200

    Updated map

--- mapping/map.py
+++ mapping/map.py
@@ -1,3 +1,8 @@
 import folium
 map = folium.Map(location=[38.58, -99.09], zoom_start=6)
+
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[38.2,-99.1], popup="Hi I am a marker", icon=folium.Icon(color="blue")))
+map.add_child(fg)
+
 map.save("mapping/map.html")
